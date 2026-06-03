7bb873636b5ea51ff99d76f6043b4e1f61b60a1f
Noya06 <miniaturems06@gmail.com>
2026-06-03 18:04:32 +0900

    feat: ✨ 配信元のPCのデータを送信するように変更

--- multi_comm/multi_comm/comm.py
+++ multi_comm/multi_comm/comm.py
@@ -1,6 +1,26 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import socket
+import platform
+
+
+def get_pc_info():
+    try:
+        hostname = socket.gethostname()
+        # get primary outward-facing IP
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            s.connect(('8.8.8.8', 80))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        os_info = platform.platform()
+        return f'host={hostname}, ip={ip}, os={os_info}'
+    except Exception as e:
+        return f'pc_info_error:{e}'
 
 
 class CommNode(Node):
@@ -10,12 +30,12 @@ class CommNode(Node):
         self.sub = self.create_subscription(String, 'chatter', self.callback, 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.count = 0
+        self.pc_info = get_pc_info()
 
     def timer_callback(self):
         msg = String()
-        msg.data = f'Hello {self.count} from {self.get_name()}'
+        msg.data = f'Hello {self.count} from {self.get_name()} ({self.pc_info})'
         self.pub.publish(msg)
-        self.get_logger().info(f'Publishing: "{msg.data}"')
         self.count += 1
 
     def callback(self, msg):
